ccd9538e90ee7bd633e8f62b7d2cb451953c8727
kassrojas <kassandrarojass@gmail.com>
2023-01-08 12:46:33 -0500

    switch case in serverjs, prompt questions starting

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+USE employee_db;
+
+INSERT INTO department (name)
+    VALUES 
+    ('Engineering'),
+    ('Human Resources');
+
+
+INSERT INTO role ( title, salary, department_id )
+    VALUES
+    ('Lead Engineer', 100000, 1),
+    ('Junior Engineer', 75000, 2),
+    ('HR Advisor', 60000, 3),
+    ('HR Chief Executive', 75000, 2);
+
+INSERT INTO employee ( first_name,last_name, role_id, manager_id )
+    VALUES
+    ('Sam', 'Smith', 1, null),
+    ('Tim', 'Turner', 2, 1),
+    ('Bob', 'Berry', 3, null),
+    ('Mason', 'Moody', 4, 2);
---
+++ server.js
@@ -0,0 +1,55 @@
+const inquirer = require('inquirer');
+const prompt = inquirer.createPromptModule();
+const mysql = require('mysql2');
+require('console.table');
+
+const db = mysql.createConnection({
+    user: 'root',
+    database: 'employee_db',
+});
+
+const chooseOption = (type) => {
+    
+    switch(type){
+        case 'VIEW ALL EMPLOYEES': {
+            db.query('SELECT * FROM employee', (err, employees) => {
+                console.table(employees);
+                init();
+            });
+            break;
+        }
+        case 'VIEW ALL DEPARTMENTS': {
+            db.query('SELECT * FROM department', (err, deparments) => {
+                console.table(deparments);
+                init();
+            });
+            break;
+        }
+        case 'VIEW ALL ROLES': {
+            db.query('SELECT * FROM role', (err, roles) => {
+                console.table(roles);
+                init();
+            });
+            break;
+        }
+    }
+}
+
+const init = () => {
+    
+    prompt ({
+        type: 'rawlist',
+        message: 'Choose one of the following',
+        choices: [
+            'VIEW ALL EMPLOYEES',
+            'VIEW ALL DEPARTMENTS',
+            'VIEW ALL ROLES',
+        ],
+        name: 'type',
+    })
+    .then((answers) => {
+        chooseOption(answers.type);
+    });
+};
+
+init();
